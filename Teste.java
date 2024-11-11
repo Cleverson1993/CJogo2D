e37d0eb8ca97316f25a1df8a7e994163a1d5f2a2
Cleverson1993 <163061546+Cleverson1993@users.noreply.github.com>
2024-11-10 21:02:47 -0300

    pergunta nome do usuário e comprimenta

---
+++ Teste.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);//Cria um objeto Scanner para ler a entrada do usuário;
+
+        System.out.println("Qual é se nome?");//pergunta ao usuário;
+        String nome = scanner.nextLine();//Lê a linha de entrada do usuário e armazenana variavel 'nome';
+
+        System.out.println("Olá, " + nome + "!");//Imprime uma saudação usando o nome fornecido pelo usuário;
+
+    }
+
+
+    
+}
